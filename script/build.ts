1bfad2d4f55581b5fc156973444553e4e95698be
dwgilworship-prog <dw.gilworship@gmail.com>
2025-12-12 14:06:41 +0900

    Update build.ts

--- script/build.ts
+++ script/build.ts
@@ -1,9 +1,8 @@
 import { build as esbuild } from "esbuild";
 import { build as viteBuild } from "vite";
 import { rm, readFile } from "fs/promises";
+import path from "path";
 
-// server deps to bundle to reduce openat(2) syscalls
-// which helps cold start times
 const allowlist = [
   "@google/generative-ai",
   "axios",
@@ -58,6 +57,9 @@ async function buildAll() {
     minify: true,
     external: externals,
     logLevel: "info",
+    alias: {
+      "@shared": path.resolve("shared"),
+    },
   });
 }
 
